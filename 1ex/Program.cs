a2b097d5ebffd5743289a883246d0aef56d70034
Nastya <treninanst@gmail.com>
2023-06-22 17:53:34 +0400

    Added commit

---
+++ 1ex/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    int num = int.Parse(Console.ReadLine()!);
+    return num;
+}
+int A = Prompt ("Введите первое число");
+int B = Prompt ("Введите второе число");
+int FindProduct (int a, int b)
+{
+    int rez = A;
+        for ( int i = 1; i < b; i ++)
+        {
+        rez *= A;
+        }
+    return rez;
+}
+int result = FindProduct(A,B);
+Console.WriteLine($" {A}, {B} -> {result}");
